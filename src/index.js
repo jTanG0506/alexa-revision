ce39aa19750e5eb9f6e8ffa568f7cf5ef2f146a4
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-23 18:54:57 +0100

    Add handler to give definition of a keyword

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 var Alexa = require('alexa-sdk');
 
+// Handlers
+var definitionStateHandlers = require('./handlers/definitionStateHandlers');
+
 exports.handler = function(event, context, callback){
   var alexa = Alexa.handler(event, context);
-  alexa.registerHandlers(handlers);
+  alexa.registerHandlers(
+    definitionStateHandlers
+  );
+  alexa.state = '_DEFINITION';
   alexa.execute();
-};
-
-// Define handler functions below.
-var handlers = {
-  'LaunchRequest': function () {
-    this.emit(':ask', 'Welcome to my template code!', 'Try saying hello!');
-  }
 };
